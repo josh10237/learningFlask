42a1823e2dbd5e7c431c6a85d1070e7c8e31df9a
Josh Benson <josh.benson.805@gmail.com>
2020-02-07 12:03:34 -0800

    Finished login and register starting vid 4

--- pythonblog.py
+++ pythonblog.py
@@ -1,52 +1,21 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
-
-app.config['SECRET_KEY'] = 'o3houx9pns4e1imzbv54'
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
 posts = [
     {
-        'author': 'Josh Benson',
+        'author': 'Corey Schafer',
         'title': 'Blog Post 1',
-        'content': 'New content',
-        'date_posted': 'Febuary 4, 2020'
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
     },
     {
-        'author': 'Josh Smenson',
+        'author': 'Jane Doe',
         'title': 'Blog Post 2',
-        'content': 'Newer content',
-        'date_posted': 'Febuary 5, 2020'
-    },
-    {
-        'author': 'El Simon',
-        'title': 'Blog Post 3',
-        'content': 'Hot take: Pinapple on pizza is good. CENSOR*D.',
-        'date_posted': 'Febuary 7, 2020'
-    },
-    {
-        'author': 'Anonamous',
-        'title': 'Blog Post 5',
-        'content': 'Hot take: Self described centrists are not any better than conservatives',
-        'date_posted': 'Febuary 7, 2020'
-    },
-    {
-        'author': 'Pee pee poo poo',
-        'title': 'Blog Post 6',
-        'content': 'Hot take: I dont care about the news?',
-        'date_posted': 'Febuary 7, 2020'
-    },
-    {
-        'author': 'Pee pee poo poos twin sister',
-        'title': 'Blog Post 7',
-        'content': 'Hot take: Everytime Pr***k and ****ie leave they are hooking up',
-        'date_posted': 'Febuary 7, 2020'
-    },
-    {
-        'author': 'Protik',
-        'title': 'Blog Post 8',
-        'content': 'Hot take: Names without the letter "n" are better than names that contain the letter "n." Hobbes. Case in Point',
-        'date_posted': 'Febuary 7, 2020'
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
     }
 ]
 
@@ -61,15 +30,26 @@ def home():
 def about():
     return render_template('about.html', title='About')
 
-@app.route("/register")
+
+@app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
-    return render_template('register.html', title='Sign Up', form=form)
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
 
-@app.route("/login")
-def register():
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
     form = LoginForm()
-    return render_template('login.html', title='Log In', form=form)
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
