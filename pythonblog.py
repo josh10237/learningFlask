27636d0a4bcd6cfc563ff8b48d3036afcc5d0ae5
Josh Benson <josh.benson.805@gmail.com>
2020-02-07 10:02:05 -0800

    Woking on Login page (PART WAY THROUGH VID 3)

--- pythonblog.py
+++ pythonblog.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'o3houx9pns4e1imzbv54'
 
 posts = [
     {
@@ -13,6 +17,36 @@ posts = [
         'title': 'Blog Post 2',
         'content': 'Newer content',
         'date_posted': 'Febuary 5, 2020'
+    },
+    {
+        'author': 'El Simon',
+        'title': 'Blog Post 3',
+        'content': 'Hot take: Pinapple on pizza is good. CENSOR*D.',
+        'date_posted': 'Febuary 7, 2020'
+    },
+    {
+        'author': 'Anonamous',
+        'title': 'Blog Post 5',
+        'content': 'Hot take: Self described centrists are not any better than conservatives',
+        'date_posted': 'Febuary 7, 2020'
+    },
+    {
+        'author': 'Pee pee poo poo',
+        'title': 'Blog Post 6',
+        'content': 'Hot take: I dont care about the news?',
+        'date_posted': 'Febuary 7, 2020'
+    },
+    {
+        'author': 'Pee pee poo poos twin sister',
+        'title': 'Blog Post 7',
+        'content': 'Hot take: Everytime Pr***k and ****ie leave they are hooking up',
+        'date_posted': 'Febuary 7, 2020'
+    },
+    {
+        'author': 'Protik',
+        'title': 'Blog Post 8',
+        'content': 'Hot take: Names without the letter "n" are better than names that contain the letter "n." Hobbes. Case in Point',
+        'date_posted': 'Febuary 7, 2020'
     }
 ]
 
@@ -26,6 +60,16 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Sign Up', form=form)
+
+@app.route("/login")
+def register():
+    form = LoginForm()
+    return render_template('login.html', title='Log In', form=form)
 
 
 if __name__ == '__main__':
